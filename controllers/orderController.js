42364ac61fe0359f3b898fa9fa7faf554b9305a6
Trương Pháp <truongphapdh@gmail.com>
2020-06-23 21:41:25 +0700

    update product

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,22 +1,34 @@
 const Users = require("../models/user");
 const Order = require("../models/Order");
+const CartItem = require("../models/cartItem");
+const Product = require("../models/Product");
 const bodyParser = require("body-parser");
 
 module.exports.index = async (req, res) => {
   res.send("OrderController");
 };
 module.exports.postNewOrder = async (req, res) => {
-  const total = req.body.products.reduce((a, b) => {
+  const items = await CartItem.find({ userId: req.user._id }).populate({
+    path: "productId",
+    select: "price",
+    model: Product,
+  });
+  const cart = items.map(x => {
+    return {
+      productId: x.productId._id,
+      quantity: x.count,
+      price: x.productId.price,
+    };
+  });
+  const total = cart.reduce((a, b) => {
     return a.price * a.quantity + b.price * b.quantity;
   });
+
   const order = new Order({
     userId: req.user._id,
     email: req.body.email,
-    products: req.body.products,
+    products: cart,
     total: total,
-    // total: req.body.products.reduce((a, b) => {
-    //   return a.price * a.quantity + b.price * b.quantity;
-    // }),
   });
   console.log(order);
 
