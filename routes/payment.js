9ef09c1b03ceee89264ff6dc838309c532000036
Trương Pháp <truongphapdh@gmail.com>
2020-07-07 20:23:30 +0700

    update product & payment

--- routes/payment.js
+++ routes/payment.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 const controller = require("../controllers/paymentController");
-router.get("/", controller.index);
+router.post("/", controller.index);
 router.post("/resolve", controller.resolve);
 module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -5,7 +5,7 @@ const controller = require("../controllers/productController");
 
 router.get("/", controller.index);
 router.post("/", controller.post);
-router.get("/search", controller.search);
+router.post("/search", controller.search);
 router.delete("/:prodID", controller.delete);
 router.patch("/:prodID", controller.update);
 router.get("/:prodID", controller.getOne);
